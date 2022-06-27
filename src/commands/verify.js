c1028d82636ef3b6766cd6d27b764f057abfadb3
tunogya <tunogya@qq.com>
2022-06-27 22:40:09 +0800

    udpate

---
+++ src/commands/verify.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('verify')
+		.setDescription('Verify your crypto assets'),
+	async execute(interaction) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Verify your assets')
+			.setURL('https://discord.js.org')
+			.setDescription('This is a read-only connection. Do not share your private keys. We will never ask for your seed phrase. We will never DM you.');
+
+		await interaction.reply({ components: [row], embeds: [embed] });
+	},
+};
