1b23e30cc3a366e0be04896d3fe8c3712ee2e60c
tunogya <tunogya@qq.com>
2022-06-27 22:57:26 +0800

    udpate

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -6,26 +6,26 @@ module.exports = {
 		.setName('verify')
 		.setDescription('Verify your crypto assets'),
 	async execute(interaction) {
-		const verifyBtn = new MessageActionRow()
-			.addComponents(
+		const row = new MessageActionRow()
+			.addComponents([
 				new MessageButton()
 					.setCustomId('verify')
 					.setLabel('Let\'s go')
 					.setStyle('PRIMARY'),
-			);
-		const docBtn = new MessageActionRow()
-			.addComponents(
 				new MessageButton()
-					.setCustomId('doc')
-					.setLabel('Doc')
-					.setStyle('LINK'),
-			);
+					.addComponents(
+						new MessageButton()
+							.setCustomId('doc')
+							.setLabel('Doc')
+							.setStyle('LINK'),
+					),
+			]);
 		const embed = new MessageEmbed()
 			.setAuthor('Wakanda+')
 			.setTitle('Verify your assets')
 			.setURL('https://discord.js.org')
 			.setDescription('This is a read-only connection. Do not share your private keys. We will never ask for your seed phrase. We will never DM you.');
 
-		await interaction.reply({ components: [verifyBtn, docBtn], embeds: [embed] });
+		await interaction.reply({ components: [row], embeds: [embed] });
 	},
 };
