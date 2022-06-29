bf86d4c7cfe35982e8b549c3e4b138011732c02c
tunogya <tunogya@qq.com>
2022-06-29 12:21:54 +0800

    fixed

--- src/events/toConnectWallet.js
+++ src/events/toConnectWallet.js
@@ -1,7 +1,7 @@
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'toConnectWallet',
+	name: 'interactionCreate',
 	async execute(interaction) {
 		if (!interaction.isButton()) return;
 		if (interaction.customId === 'toConnectWallet') {
