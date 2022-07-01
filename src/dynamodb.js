f5a1b05b94b93c3141f918a997d1cbb00f364091
tunogya <tunogya@qq.com>
2022-07-02 01:01:51 +0800

    update

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,5 +1,9 @@
 const AWS = require('aws-sdk');
 
-const dynamo = new AWS.DynamoDB.DocumentClient({ region: 'ap-northeast-1' })
+const dynamo = new AWS.DynamoDB.DocumentClient({
+	region: 'ap-northeast-1',
+	accessKeyId: process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_ACCESS_SECRET,
+});
 
 module.exports = dynamo;
