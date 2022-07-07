e04bb39501ce8ffcfccefe2429fec3357d1450c9
tunogya <tunogya@qq.com>
2022-07-07 18:57:37 +0800

    update

--- src/commands/connectWallet.js
+++ src/commands/connectWallet.js
@@ -1,45 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
-const randomString = require('../utils/randomString.js');
-const client = require('../libs/redis.js');
-const { setTimeout: wait } = require('node:timers/promises');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('connectwallet')
 		.setDescription('Connect a new wallet'),
 	async execute(interaction) {
-		const user = interaction.user.id;
-		const state = randomString(12);
-		const message = `My discord is ${interaction.user.tag} and i want to connect my wallet in Wakanda Metaverse. ${new Date().toISOString()}`
-		await client.set(
-			state,
-			JSON.stringify({
-				user: user,
-				message: message
-			}),
-			{
-				EX: 300,
-			}
-		);
-		
 		const row = new MessageActionRow().addComponents(
 			new MessageButton()
-				.setLabel('Connect Wallet')
-				.setURL(`https://wakandaplus.wakanda.cn/#/${state}`)
-				.setStyle('LINK')
+				.setCustomId('toConnectWallet')
+				.setLabel("Let's go")
+				.setStyle('PRIMARY')
 		);
 		const embed = new MessageEmbed()
-			.setTitle('Please connect your wallet in 5 min.')
+			.setTitle('Connect a new wallet')
 			.setDescription(
-				'You should expect to sign the following message when prompted by a non-custodial wallet such as MetaMask.\nMake sure you sign the EXACT message and NEVER share your seed phrase or private key.'
+				'This is a read-only connection. Do not share your private keys. We will never ask for your seed phrase.'
 			);
 		
 		await interaction.reply({
-			content: message,
 			components: [row],
 			embeds: [embed],
 			ephemeral: true,
 		});
-	}
+	},
 };
