12113ea5481ecac1a583ff45df1c9c2ee6305c67
tunogya <tunogya@qq.com>
2022-07-07 16:30:58 +0800

    update

--- src/commands/myWallets.js
+++ src/commands/myWallets.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const redisClient = require('../libs/redis.js');
 const { getUser } = require('../dynamodb/wakandaplus.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,23 +9,24 @@ module.exports = {
 		.setDescription('Manage your wallets'),
 	async execute(interaction) {
 		const user = interaction.user;
-		// 通过缓存查询用户的雪花号
 		const id = await redisClient.get(user.id);
 		if (id) {
 			const q = await getUser(id);
 			const info = q.Item;
 			if (info) {
 				const wallets = info['wallets'] ? Array.from(info['wallets']) : [];
-				// 换成罗列用户的所有地址，并可以对其进行管理
+				const row = new MessageActionRow()
+					.addComponents(
+						wallets.map((item) => new MessageButton().setLabel(item).setStyle('SECONDARY'))
+					);
 				await interaction.reply({
-					content: `${user.username}'s wallets are here.
-
-${wallets.length > 0 ? `${wallets.join('\n')}` : 'None address here.'}
-`,
+					content: `${user.username}'s wallets are here. \n${wallets.length > 0 ? `${wallets.join('\n')}` : 'None address here.'}`,
+					components: [row],
 					ephemeral: true,
 				});
 			}
-		} else {
+		}
+		else {
 			// 通过数据库查询是否真的没有用户数据
 			// 如果没有，则创建新的用户记录
 			await interaction.reply({
