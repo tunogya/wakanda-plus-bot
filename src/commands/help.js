c55147b924cc9ac0244d7b62fd92dc8d1f757185
tunogya <tunogya@qq.com>
2022-07-08 08:22:08 +0800

    update

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,29 +1,33 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription(`Get effective help from bot`),
 	async execute(interaction) {
-		const embed = new MessageEmbed()
-			.setTitle('I can help you connect and manage wallets. You can control me by sending these commands:')
-			.setDescription(
-				`**Wallets**
+		await interaction.reply({
+			content: `I can help you connect and manage wallets.
+
+You can control me by sending these commands:
+
+**Wallets**
+\`\`\`
 */connectwallet* - connect a new wallet
 */mywallets* - manage your wallets
 */deletewallet* - delete a wallet
+\`\`\`
 
 **Role**
+\`\`\`
 */claimrole* - claim role manually
 */revokerole* - revoke a role
+\`\`\`
 
 **Other**
-*/cancel* - cancel the current operation`
-			);
-		
-		await interaction.reply({
-			embeds: [embed],
+\`\`\`
+*/cancel* - cancel the current operation
+\`\`\`
+`,
 			ephemeral: true,
 		});
 	},
