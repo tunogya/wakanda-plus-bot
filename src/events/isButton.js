921dde148bc3a00316d7ca582e79639276f32a20
tunogya <tunogya@qq.com>
2022-07-08 10:47:29 +0800

    update

--- src/events/isButton.js
+++ src/events/isButton.js
@@ -12,8 +12,8 @@ module.exports = {
 		if (interaction.customId === 'toConnectWallet') {
 			const user = interaction.user.id;
 			const state = randomString(12);
-			const message = `Please sign the message in 5 min:
-My account is ${interaction.user.tag}. I want to connect my wallet in Wakanda Metaverse. ${new Date().toLocaleString()}
+			const message = `**Please sign the message below in 5 min**
+> My account is ${interaction.user.tag}. I want to connect my wallet in Wakanda Metaverse. ${new Date().toLocaleString()}
 
 [https://wakandaplus.wakanda.cn/#/${state}](https://wakandaplus.wakanda.cn/#/${state})`
 			await client.set(
