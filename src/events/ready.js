4e799710ee79bbbd36e0a0798ee1bbb4c4255920
tunogya <tunogya@qq.com>
2022-07-09 11:29:28 +0800

    update

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,20 +5,5 @@ module.exports = {
 	once: true,
 	execute(client) {
 		log.info(`Ready! Logged in as ${client.user.tag}`)
-		const channel = client.channels.cache.get('989759769621434418');
-		channel.send(`I can help you connect and manage wallets.
-You can control me by sending these commands:
-
-*/connectwallet* - connect a new wallet
-*/mywallets* - manage your wallets
-
-**Edit Wallets**
-*/deletewallet* - delete a wallet
-
-**Role**
-*/claimrole* - claim role manually
-
-**Other**
-*/cancel* - cancel the current operation`);
 	},
 };
