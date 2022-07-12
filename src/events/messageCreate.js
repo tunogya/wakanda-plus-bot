f228ec0b8c859555f2f25f264b0fcb87f00c1429
tunogya <tunogya@qq.com>
2022-07-12 14:42:02 +0800

    Revert "update"
    This reverts commit b79739f1874b1a7150c311daf435ad41af33afd1.

--- src/events/messageCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	name: 'messageCreate',
-	async execute(interaction) {
-		console.log(interaction)
-	}
-}
--- src/index.js
+++ src/index.js
@@ -27,44 +27,36 @@ for (const file of commandFiles) {
 	bot.commands.set(command.data.name, command);
 }
 
-bot.once('ready', c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+	.readdirSync(eventsPath)
+	.filter((file) => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		bot.once(event.name, (...args) => event.execute(...args));
+	} else {
+		bot.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+bot.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const command = bot.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.log(error);
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true,
+		});
+	}
 });
-
-bot.on('interactionCreate', interaction => {
-	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-});
-
-// const eventsPath = path.join(__dirname, 'events');
-// const eventFiles = fs
-// 	.readdirSync(eventsPath)
-// 	.filter((file) => file.endsWith('.js'));
-// for (const file of eventFiles) {
-// 	const filePath = path.join(eventsPath, file);
-// 	const event = require(filePath);
-// 	if (event.once) {
-// 		bot.once(event.name, (...args) => event.execute(...args));
-// 	} else {
-// 		bot.on(event.name, (...args) => event.execute(...args));
-// 	}
-// }
-
-// bot.on('interactionCreate', async (interaction) => {
-// 	if (!interaction.isCommand()) return;
-//
-// 	const command = bot.commands.get(interaction.commandName);
-//
-// 	if (!command) return;
-//
-// 	try {
-// 		await command.execute(interaction);
-// 	} catch (error) {
-// 		console.log(error);
-// 		await interaction.reply({
-// 			content: 'There was an error while executing this command!',
-// 			ephemeral: true,
-// 		});
-// 	}
-// });
 
 bot.login(token);
