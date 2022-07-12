ced0920684ed950e3e65476efba00cf7a5167465
tunogya <tunogya@qq.com>
2022-07-12 19:06:38 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,68 +7,36 @@ module.exports = {
 		// only support guild(Wakanda Metaverse) messages
 		if (message.guildId !== '980009405401677854') return;
 		// support messages from 'davinci' channel
-		if (message.channelId === '996278471422660688') {
-			const res = await openai.createCompletion({
-				model: 'text-davinci-002',
-				prompt: '```' + message.content + '```',
-				temperature: 0.9,
-				top_p: 1,
-				max_tokens: 100,
-				frequency_penalty: 0,
-				presence_penalty: 0.6,
-			})
-			
-			await message.reply({
-				content: res.data.choices[0].text,
-			});
+		let model;
+		switch (message.channelId) {
+		case '996278471422660688':
+			model = 'text-davinci-002';
+			break;
+		case '996280015379509288':
+			model = 'text-curie-001';
+			break;
+		case '996280421283266733':
+			model = 'text-babbage-001';
+			break;
+		case '996280490237632622':
+			model = 'text-ada-001';
+			break;
+		default:
+			return;
 		}
-		// support messages from 'curie' channel
-		else if (message.channelId === '996280015379509288') {
-			const res = await openai.createCompletion({
-				model: 'text-curie-001',
-				prompt: '```' + message.content + '```',
-				temperature: 0.9,
-				top_p: 1,
-				max_tokens: 150,
-				frequency_penalty: 0,
-				presence_penalty: 0.6,
-			})
-			
-			await message.reply({
-				content: res.data.choices[0].text,
-			});
-		}
-		// support messages from 'babbage' channel
-		else if (message.channelId === '996280421283266733') {
-			const res = await openai.createCompletion({
-				model: 'text-babbage-001',
-				prompt: '```' + message.content + '```',
-				temperature: 0.9,
-				top_p: 1,
-				max_tokens: 150,
-				frequency_penalty: 0,
-				presence_penalty: 0.6,
-			})
-			
-			await message.reply({
-				content: res.data.choices[0].text,
-			});
-		}
-		// support messages from 'ada' channel
-		else if (message.channelId === '996280490237632622') {
-			const res = await openai.createCompletion({
-				model: 'text-ada-001',
-				prompt: '```' + message.content + '```',
-				temperature: 0.9,
-				top_p: 1,
-				max_tokens: 150,
-				frequency_penalty: 0,
-				presence_penalty: 0.6,
-			})
-			
-			await message.reply({
-				content: res.data.choices[0].text,
-			});
-		}
+		
+		const res = await openai.createCompletion({
+			model: model,
+			prompt: message.content,
+			temperature: 0.9,
+			top_p: 1,
+			max_tokens: 100,
+			frequency_penalty: 0,
+			presence_penalty: 0.6,
+		});
+		
+		await message.reply({
+			content: '```' + res.data.choices[0].text + '```',
+		});
 	},
 };
