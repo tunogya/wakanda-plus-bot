7466d472a392186276970bedbaf7ffabadeec8b6
tunogya <tunogya@qq.com>
2022-07-12 18:30:27 +0800

    update prune to guild command

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -10,14 +10,10 @@ const guildId = process.env.guildId;
 const token = process.env.token;
 
 const commands = [];
-const guildCommands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs
 	.readdirSync(commandsPath)
 	.filter((file) => file.endsWith('.js'));
-const guildCommandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.guild.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -25,22 +21,11 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-for (const file of guildCommandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	guildCommands.push(command.data.toJSON());
-}
-
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest
-	.put(Routes.applicationCommands(clientId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.log);
-
-rest
 	.put(Routes.applicationGuildCommands(clientId, guildId), {
-		body: guildCommands,
+		body: commands,
 	})
 	.then(() =>
 		console.log('Successfully registered application guild commands.')
