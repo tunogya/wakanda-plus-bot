f6091bd158c9834c841652307e9d546c96fcea64
tunogya <tunogya@qq.com>
2022-07-13 14:05:02 +0800

    update

--- src/commands/cancel.js
+++ src/commands/cancel.js
@@ -6,10 +6,10 @@ module.exports = {
 		.setName('cancel')
 		.setDescription('Cancel a command.'),
 	async execute(interaction) {
-		const res = await redisClient.get(`${interaction.guild.id}-${interaction.channelId}-${interaction.user.id}-intention`);
+		const res = await redisClient.get(`${interaction.guildId}-${interaction.channelId}-${interaction.user.id}-intention`);
 		if (res) {
 			try {
-				await redisClient.del(`${interaction.guild.id}-${interaction.channelId}-${interaction.user.id}-intention`);
+				await redisClient.del(`${interaction.guildId}-${interaction.channelId}-${interaction.user.id}-intention`);
 				interaction.reply({ content: 'Command cancelled.', ephemeral: true });
 			} catch (e) {
 				interaction.reply({ content: 'There was an error trying to cancel the command.', ephemeral: true });
