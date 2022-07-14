c769b539a26d7a7f4afa05e6135895f6e411ebae
tunogya <tunogya@qq.com>
2022-07-14 21:24:39 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,16 +8,15 @@ module.exports = {
 		// check if the message is in a channel that is in the list of channels that we want to listen to
 		const intention = redisClient.get(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
 		if (intention) {
-			const intent = JSON.parse(intention);
 			const q = await openai.createCompletion({
-				model: intent.model,
+				model: intention.model,
 				prompt: message.content,
-				temperature: intent.temperature,
-				top_p: intent.top_p,
-				max_tokens: intent.max_tokens,
-				frequency_penalty: intent.frequency_penalty,
-				presence_penalty: intent.presence_penalty,
-				best_of: intent.best_of
+				temperature: intention.temperature,
+				top_p: intention.top_p,
+				max_tokens: intention.max_tokens,
+				frequency_penalty: intention.frequency_penalty,
+				presence_penalty: intention.presence_penalty,
+				best_of: intention.best_of
 			});
 			await message.reply({
 				content: q.choices[0].text,
