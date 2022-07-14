401fb4c4a09435c2361819ad5dcfdf9c072ae106
tunogya <tunogya@qq.com>
2022-07-14 22:42:16 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -27,7 +27,7 @@ module.exports = {
 			
 			const embed = new MessageEmbed()
 					.setTitle('Sponsors Overview')
-					.setDescription(`${res.headers['openai-organization'].toUpperCase()} already sponsored ${sponsorships} ${sponsorships > 1 ? 'times' : 'time'} in this guild.
+					.setDescription(`*${res.headers['openai-organization'].toUpperCase()} already sponsored ${sponsorships} ${sponsorships > 1 ? 'times' : 'time'}* in this guild.
 
 Everyone can sponsor this AI bot if you have access to OpenAI's API.\nWe are very much looking forward to the DALL-E 2 model joining the community.`);
 			await message.reply({
