c3a2b08d16414862c8fe2a88b23d418a682c1845
tunogya <tunogya@qq.com>
2022-07-14 22:30:39 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,33 +7,33 @@ module.exports = {
 	async execute(message) {
 		if (message.author.bot) return
 		// check if the message is in a channel that is in the list of channels that we want to listen to
-		const intention = redisClient.get(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
-		console.log(intention);
+		const intention = await redisClient.get(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
 		if (intention) {
-			// const res = await openai.createCompletion({
-			// 	model: intention.model,
-			// 	prompt: message.content,
-			// 	temperature: intention.temperature,
-			// 	top_p: intention.top_p,
-			// 	max_tokens: intention.max_tokens,
-			// 	frequency_penalty: intention.frequency_penalty,
-			// 	presence_penalty: intention.presence_penalty,
-			// 	best_of: intention.best_of
-			// });
+			const intentionObj = JSON.parse(intention);
+			const res = await openai.createCompletion({
+				model: intentionObj.model,
+				prompt: intentionObj.content,
+				temperature: intentionObj.temperature,
+				top_p: intentionObj.top_p,
+				max_tokens: intentionObj.max_tokens,
+				frequency_penalty: intentionObj.frequency_penalty,
+				presence_penalty: intentionObj.presence_penalty,
+				best_of: intentionObj.best_of
+			});
 			await redisClient.del(`${message.guildId}-${message.channelId}-${message.author.id}-intention)`);
-			// const org = res.config.headers['OpenAI-Organization']
-			//
-			// const sponsorships = await redisClient.incr(`${org}-sponsorships`)
-			const sponsorships = 1
+			const org = res.config.headers['OpenAI-Organization']
+
+			const sponsorships = await redisClient.incr(`${org}-sponsorships`)
 			
 			const embed = new MessageEmbed()
 					.setTitle('Sponsors Overview')
-					.setDescription(`
-Everyone can sponsor this AI bot if you have access to OpenAI's API. We are very much looking forward to the DALL-E 2 model joining the community.`);
+					.setDescription(`Sponsor: ${res.headers['openai-organization'].toUpperCase()}
 
+${res.headers['openai-organization'].toUpperCase()} already sponsored ${sponsorships} ${sponsorships > 1 ? 'times' : 'time'}.
+
+Everyone can sponsor this AI bot if you have access to OpenAI's API. We are very much looking forward to the DALL-E 2 model joining the community.`);
 			await message.reply({
-				// content: res.data.choices[0].text,
-				content: `${intention.model}, ${message.content}, ${intention.temperature}, ${intention.top_p}, ${intention.max_tokens}, ${intention.frequency_penalty}, ${intention.presence_penalty}, ${intention.best_of}`,
+				content: res.data.choices[0].text,
 				embeds: [embed],
 			})
 		}
