f571831dfcefdc134bb3a4fccd1e399059a31337
tunogya <tunogya@qq.com>
2022-07-16 23:09:03 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
 		if (message.author.bot) return
-		
+		message.reply('Wait a second, I am still learning...');
 		// check if the message is in a channel that is in the list of channels that we want to listen to
 		const intention = await redisClient.get(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
 		if (intention) {
@@ -27,7 +27,7 @@ module.exports = {
 			
 			await redisClient.del(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
 			
-			await message.reply({ content: response.data.choices.map(item => item.text).join('\n')});
+			await message.edit({ content: response.data.choices.map(item => item.text).join('\n')});
 		}
 	},
 };
