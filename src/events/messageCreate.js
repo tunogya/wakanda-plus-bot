0bdc961573423b9cc29855f7432275a7c42bad03
tunogya <tunogya@qq.com>
2022-07-16 19:01:50 +0800

    update

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -23,6 +23,7 @@ module.exports = {
 				n: intentionObj.n,
 				suffix: intentionObj.suffix,
 				echo: intentionObj.echo,
+				stop: intentionObj.stop,
 			});
 			await redisClient.del(`${message.guildId}-${message.channelId}-${message.author.id}-intention`);
 			const org = res.config.headers['OpenAI-Organization']
