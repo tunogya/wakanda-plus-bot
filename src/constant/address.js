7c57c863ea7cb96096121f648027c760889a5d8b
tunogya <tunogya@qq.com>
2022-07-20 12:44:51 +0800

    update mumbai Pass

--- src/constant/address.js
+++ src/constant/address.js
@@ -4,7 +4,7 @@ const WAKANDAPASS_ADDRESS = {
 	[SupportedChainId.MAINNET]: '',
 	[SupportedChainId.RINKEBY]: '0x863a31036c9cd2d82da29eb34Ba4422C3B2FF51F',
 	[SupportedChainId.POLYGON]: '',
-	[SupportedChainId.POLYGON_MUMBAI]: '',
+	[SupportedChainId.POLYGON_MUMBAI]: '0x188cE78bcE46C958Bb287e0A57f5B68C4cC9632a',
 }
 
 module.exports = {
