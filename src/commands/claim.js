4b5ed42063afd043b61e571287d66f9ba060255c
tunogya <tunogya@qq.com>
2022-07-24 15:32:52 +0800

    update

---
+++ src/commands/claim.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { RinkebyProvider, MumbaiProvider, GoerliProvider } = require("../libs/web3");
+const ethers = require("ethers");
+const { WAKANDAPASS_ADDRESS } = require("../constant/address");
+const SupportedChainId = require("../constant/chains");
+const geohash_abi = require("../abis/geohash.json");
+const { getExplorerLink, ExplorerDataType } = require("../utils/getExplorerLink");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+			.setName('claim')
+			.setDescription('Claim a PASS.'),
+	async execute(interaction) {
+		const rinkebyPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.RINKEBY], geohash_abi, RinkebyProvider)
+		const mumbaiPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON_MUMBAI], geohash_abi, MumbaiProvider)
+		const goerliPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], geohash_abi, GoerliProvider)
+		
+		const rinkebyBalance = await rinkebyPassContract.balanceOf(WAKANDAPASS_ADDRESS[SupportedChainId.RINKEBY])
+		const mumbaiBalance = await mumbaiPassContract.balanceOf(WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON_MUMBAI])
+		const goerliBalance = await goerliPassContract.balanceOf(WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI])
+		
+		await interaction.reply({
+			content: `Rinkeby: ${rinkebyBalance.toNumber()} PASS could be claimed.
+Click here to claim: *${getExplorerLink(SupportedChainId.RINKEBY, WAKANDAPASS_ADDRESS[SupportedChainId.RINKEBY], ExplorerDataType.TOKEN)}*
+
+Mumbai: ${mumbaiBalance.toNumber()} PASS could be claimed.
+Click here to claim: *${getExplorerLink(SupportedChainId.POLYGON_MUMBAI, WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON_MUMBAI], ExplorerDataType.TOKEN)}*
+
+Goerli: ${goerliBalance.toNumber()} PASS could be claimed.
+Click here to claim: *${getExplorerLink(SupportedChainId.GOERLI, WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], ExplorerDataType.TOKEN)}*
+`,
+			ephemeral: true
+		})
+	},
+};
