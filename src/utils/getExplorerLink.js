86c281bf639a8007e3612cbc6f7a289b1ff36b32
tunogya <tunogya@qq.com>
2022-07-24 17:49:53 +0800

    delete unused network

--- src/utils/getExplorerLink.js
+++ src/utils/getExplorerLink.js
@@ -2,11 +2,7 @@ const SupportedChainId = require("../constant/chains");
 
 const ETHERSCAN_PREFIXES = {
   [SupportedChainId.MAINNET]: 'https://etherscan.io',
-  [SupportedChainId.RINKEBY]: 'https://rinkeby.etherscan.io',
   [SupportedChainId.GOERLI]: 'https://goerli.etherscan.io',
-  [SupportedChainId.OPTIMISM]: 'https://optimistic.etherscan.io',
-  [SupportedChainId.OPTIMISTIC_KOVAN]: 'https://kovan-optimistic.etherscan.io',
-  [SupportedChainId.POLYGON_MUMBAI]: 'https://mumbai.polygonscan.com',
   [SupportedChainId.POLYGON]: 'https://polygonscan.com',
 }
 
@@ -24,34 +20,6 @@ const ExplorerDataType = {
  * @param type the type of the data
  */
 function getExplorerLink(chainId, data, type) {
-  if (chainId === SupportedChainId.ARBITRUM_ONE) {
-    switch (type) {
-      case ExplorerDataType.TRANSACTION:
-        return `https://arbiscan.io/tx/${data}`
-      case ExplorerDataType.ADDRESS:
-      case ExplorerDataType.TOKEN:
-        return `https://arbiscan.io/address/${data}`
-      case ExplorerDataType.BLOCK:
-        return `https://arbiscan.io/block/${data}`
-      default:
-        return `https://arbiscan.io/`
-    }
-  }
-
-  if (chainId === SupportedChainId.ARBITRUM_RINKEBY) {
-    switch (type) {
-      case ExplorerDataType.TRANSACTION:
-        return `https://rinkeby-explorer.arbitrum.io/tx/${data}`
-      case ExplorerDataType.ADDRESS:
-      case ExplorerDataType.TOKEN:
-        return `https://rinkeby-explorer.arbitrum.io/address/${data}`
-      case ExplorerDataType.BLOCK:
-        return `https://rinkeby-explorer.arbitrum.io/block/${data}`
-      default:
-        return `https://rinkeby-explorer.arbitrum.io/`
-    }
-  }
-
   const prefix = ETHERSCAN_PREFIXES[chainId] ?? 'https://etherscan.io'
 
   switch (type) {
@@ -62,9 +30,6 @@ function getExplorerLink(chainId, data, type) {
       return `${prefix}/token/${data}`
 
     case ExplorerDataType.BLOCK:
-      if (chainId === SupportedChainId.OPTIMISM || chainId === SupportedChainId.OPTIMISTIC_KOVAN) {
-        return `${prefix}/tx/${data}`
-      }
       return `${prefix}/block/${data}`
 
     case ExplorerDataType.ADDRESS:
