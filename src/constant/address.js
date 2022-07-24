64a2fff5514df34c3dd460d456ee91721ed853ff
tunogya <tunogya@qq.com>
2022-07-24 17:45:59 +0800

    update

--- src/constant/address.js
+++ src/constant/address.js
@@ -2,9 +2,7 @@ const SupportedChainId = require('./chains')
 
 const WAKANDAPASS_ADDRESS = {
 	[SupportedChainId.MAINNET]: '',
-	[SupportedChainId.RINKEBY]: '0xbEf929f1374138035cc86BFe03090c3bb74fF917',
 	[SupportedChainId.POLYGON]: '',
-	[SupportedChainId.POLYGON_MUMBAI]: '0x049bAA72323d1b455Ac38AA09158D140cba6baf3',
 	[SupportedChainId.GOERLI]: '0x39EfBfAa12d95082a927b55951565656848F1515',
 }
 
