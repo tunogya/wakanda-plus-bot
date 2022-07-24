5a3460c9e6d64577f066b437095354a09688358c
tunogya <tunogya@qq.com>
2022-07-24 17:47:56 +0800

    update

--- src/commands/portal.js
+++ src/commands/portal.js
@@ -9,9 +9,7 @@ module.exports = {
 			.setDescription('Portal of WakandaPass.'),
 	async execute(interaction) {
 		await interaction.reply({
-			content: `Rinkeby portal: *${getExplorerLink(SupportedChainId.RINKEBY, WAKANDAPASS_ADDRESS[SupportedChainId.RINKEBY], ExplorerDataType.TOKEN)}*
-Mumbai portal: *${getExplorerLink(SupportedChainId.POLYGON_MUMBAI, WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON_MUMBAI], ExplorerDataType.TOKEN)}*
-Goerli portal: *${getExplorerLink(SupportedChainId.GOERLI, WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], ExplorerDataType.TOKEN)}*
+			content: `Goerli portal: *${getExplorerLink(SupportedChainId.GOERLI, WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], ExplorerDataType.TOKEN)}*
 `,
 		})
 	},
--- src/constant/infura.js
+++ src/constant/infura.js
@@ -9,14 +9,8 @@ if (typeof INFURA_KEY === 'undefined') {
  */
 const INFURA_NETWORK_URLS = {
 	[SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
 	[SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
 	[SupportedChainId.POLYGON]: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
-	[SupportedChainId.POLYGON_MUMBAI]: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
 }
 
 module.exports = INFURA_NETWORK_URLS
