93e914708e94b23d32603a817bd18148ac5ff27c
tunogya <tunogya@qq.com>
2022-07-24 23:47:54 +0800

    update

--- src/commands/portal.js
+++ src/commands/portal.js
@@ -9,7 +9,7 @@ module.exports = {
 			.setDescription('Portal of WakandaPass.'),
 	async execute(interaction) {
 		await interaction.reply({
-			content: `Polygon portal: *${getExplorerLink(SupportedChainId.POLYGON, ExplorerDataType.TOKEN)}*
+			content: `Polygon portal: *${getExplorerLink(SupportedChainId.POLYGON, WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON], ExplorerDataType.TOKEN)}*
 
 *Testnet*
 Goerli portal: *${getExplorerLink(SupportedChainId.GOERLI, WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], ExplorerDataType.TOKEN)}*
