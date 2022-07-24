9537a7578edf3b2d32d61e8debfa1372ffb57998
tunogya <tunogya@qq.com>
2022-07-24 23:53:45 +0800

    update

--- src/commands/balanceof.js
+++ src/commands/balanceof.js
@@ -5,7 +5,7 @@ const ethers = require("ethers");
 const { WAKANDAPASS_ADDRESS } = require("../constant/address");
 const SupportedChainId = require("../constant/chains");
 const geohash_abi = require("../abis/geohash.json");
-const { GoerliProvider } = require("../libs/web3");
+const { GoerliProvider, PolygonProvider } = require("../libs/web3");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,13 +21,30 @@ module.exports = {
 			try {
 				const wallets = Array.from(info.wallets);
 				const goerliPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], geohash_abi, GoerliProvider)
+				const polygonPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON], geohash_abi, PolygonProvider)
 				let balanceOfGoerli = 0;
+				let balanceOfPolygon = 0;
 				let tokenURIOfGoerli = [];
+				let tokenURIOfPolygon = [];
 				for (const addr of wallets.filter(address => isAddress(address))) {
 					// query balance of address
+					const [polygonBalance] = await Promise.all([
+						polygonPassContract.tokenURI(addr),
+					]);
 					const [goerliBalance] = await Promise.all([
 						goerliPassContract.balanceOf(addr),
 					]);
+					if (polygonBalance) {
+						balanceOfPolygon += polygonBalance.toNumber();
+						let tokenIdOfPolygonPromise = [];
+						for (let i = 0; i < goerliBalance.toNumber(); i++) {
+							tokenIdOfPolygonPromise.push(polygonPassContract.tokenOfOwnerByIndex(addr, i));
+						}
+						const tokenIdOfPolygon = await Promise.all(tokenIdOfPolygonPromise);
+						const tokenURIOfPolygonPromise = tokenIdOfPolygon.map((tokenId) => polygonPassContract.tokenURI(tokenId))
+						const tokenURIs = await Promise.all(tokenURIOfPolygonPromise);
+						tokenURIOfPolygon = [...tokenURIOfPolygon, ...tokenURIs]
+					}
 					if (goerliBalance) {
 						balanceOfGoerli += goerliBalance.toNumber();
 						let tokenIdOfGoerliPromise = [];
@@ -39,6 +56,11 @@ module.exports = {
 						const tokenURIs = await Promise.all(tokenURIOfGoerliPromise);
 						tokenURIOfGoerli = [...tokenURIOfGoerli, ...tokenURIs]
 					}
+				}
+				if (balanceOfPolygon) {
+					member.roles.add('1000792723080609843')
+				} else {
+					member.roles.remove('1000792723080609843')
 				}
 				if (balanceOfGoerli) {
 					member.roles.add('999338334692327494')
