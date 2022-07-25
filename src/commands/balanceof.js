d1ad4379f772c0672aebb05fda4df4d56af88db5
tunogya <tunogya@qq.com>
2022-07-25 09:37:15 +0800

    update

--- src/commands/balanceof.js
+++ src/commands/balanceof.js
@@ -5,7 +5,7 @@ const ethers = require("ethers");
 const { WAKANDAPASS_ADDRESS } = require("../constant/address");
 const SupportedChainId = require("../constant/chains");
 const geohash_abi = require("../abis/geohash.json");
-const { PolygonProvider } = require("../libs/web3");
+const { PolygonProvider, GoerliProvider } = require("../libs/web3");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,23 +21,36 @@ module.exports = {
 			try {
 				const wallets = Array.from(info.wallets);
 				const polygonPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.POLYGON], geohash_abi, PolygonProvider)
-				let balanceOfPolygon = 0;
-				let tokenURIOfPolygon = [];
+				const goerliPassContract = new ethers.Contract(WAKANDAPASS_ADDRESS[SupportedChainId.GOERLI], geohash_abi, GoerliProvider)
+				let balanceOfPolygon = 0, balanceOfGoerli = 0;
+				let tokenURIOfPolygon = [], tokenURIOfGoerli = [];
 				for (const addr of wallets.filter(address => isAddress(address))) {
 					// query balance of address
-					const [polygonBalance] = await Promise.all([
+					const [polygonBalance, goerliBalance] = await Promise.all([
 						polygonPassContract.balanceOf(addr),
+						goerliPassContract.balanceOf(addr),
 					]);
 					if (polygonBalance.toNumber() > 0) {
 						balanceOfPolygon += polygonBalance.toNumber();
-						let tokenIdOfPolygonPromise = [];
+						let tokenIdPromises = [];
 						for (let i = 0; i < polygonBalance.toNumber(); i++) {
-							tokenIdOfPolygonPromise.push(polygonPassContract.tokenOfOwnerByIndex(addr, i));
+							tokenIdPromises.push(polygonPassContract.tokenOfOwnerByIndex(addr, i));
 						}
-						const tokenIdOfPolygon = await Promise.all(tokenIdOfPolygonPromise);
-						const tokenURIOfPolygonPromise = tokenIdOfPolygon.map((tokenId) => polygonPassContract.tokenURI(tokenId))
-						const tokenURIs = await Promise.all(tokenURIOfPolygonPromise);
+						const tokenIds = await Promise.all(tokenIdPromises);
+						const tokenURIPromises = tokenIds.map((tokenId) => polygonPassContract.tokenURI(tokenId))
+						const tokenURIs = await Promise.all(tokenURIPromises);
 						tokenURIOfPolygon = [...tokenURIOfPolygon, ...tokenURIs]
+					}
+					if (goerliBalance.toNumber() > 0) {
+						balanceOfGoerli += goerliBalance.toNumber();
+						let tokenIdPromises = [];
+						for (let i = 0; i < goerliBalance.toNumber(); i++) {
+							tokenIdPromises.push(goerliPassContract.tokenOfOwnerByIndex(addr, i));
+						}
+						const tokenIds = await Promise.all(tokenIdPromises);
+						const tokenURIPromises = tokenIds.map((tokenId) => goerliPassContract.tokenURI(tokenId))
+						const tokenURIs = await Promise.all(tokenURIPromises);
+						tokenURIOfGoerli = [...tokenURIOfGoerli, ...tokenURIs]
 					}
 				}
 				if (balanceOfPolygon) {
@@ -47,7 +60,7 @@ module.exports = {
 				}
 				await interaction.reply({
 					content: `${member.displayName.toUpperCase()} have *${balanceOfPolygon} PolygonPASS*. ${(balanceOfPolygon > 0) && (`They are: ${tokenURIOfPolygon.map((tokenURI) => `#${tokenURI}`).join(', ')}.`)}
-					
+${(`${member.displayName.toUpperCase()} have *${balanceOfGoerli} GoerliPASS*. ${(balanceOfGoerli > 0) && (`They are: ${tokenURIOfGoerli.map((tokenURI) => `#${tokenURI}`).join(', ')}.`)}`)}
 > Note: Use */balanceof Wakanda+* command can query the PASS which are NO MAN'S LAND. And then you can got them by */portal* command luckily.`,
 				});
 			} catch (e) {
