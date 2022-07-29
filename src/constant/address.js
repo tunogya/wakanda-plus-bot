a85cd40fe6206f73924abb8f521a98ad2d5d92e4
tunogya <tunogya@qq.com>
2022-07-29 15:05:12 +0800

    update

--- src/constant/address.js
+++ src/constant/address.js
@@ -1,9 +1,9 @@
 const SupportedChainId = require('./chains')
 
 const WAKANDAPASS_ADDRESS = {
-	[SupportedChainId.MAINNET]: '0x9eff416DA89df623e66e4D59f09f6dcdd44ac2c4',
-	[SupportedChainId.POLYGON]: '0x9eff416DA89df623e66e4D59f09f6dcdd44ac2c4',
-	[SupportedChainId.GOERLI]: '0x9eff416DA89df623e66e4D59f09f6dcdd44ac2c4',
+	[SupportedChainId.MAINNET]: '0x9c824c1dc64cdfcfe27c91faafc991c013bdaa74',
+	[SupportedChainId.POLYGON]: '0x9c824c1dc64cdfcfe27c91faafc991c013bdaa74',
+	[SupportedChainId.GOERLI]: '0x9c824c1dc64cdfcfe27c91faafc991c013bdaa74',
 }
 
 module.exports = {
